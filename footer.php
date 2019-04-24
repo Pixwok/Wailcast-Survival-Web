bc2390287484267ccd37273b2a6cec5eaa2f7592
Pixwok <40206880+Pixwok@users.noreply.github.com>
2019-04-24 11:46:50 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+        <a id="discord-link" target="blank" href="https://discordapp.com/invite/GHU8sPj"><img id="discord" src="img/discordlogo.png"></a>
+    <h3 id="copyright">Wailcast. Tous droits réservés.
+    <br>
+    <a id="bouton-credit" href="#">Crédit</a>
+    </h3>
+
+    <div id="module-popup" class="popup">   
+        <div class="popup-content" >
+            <span class="close-popup">&times;</span>
+            <h3 class="title-credit">Crédits</h3>
+            <div class="separator"></div>
+            <p class="credit">
+            Design: <a target="blank" href="https://twitter.com/pixwok">Pixwok</a>
+            <br/>Développement: <a target="blank" href="https://twitter.com/pixwok">Pixwok</a> et <a target="blank" href="https://twitter.com/Ysti__">Ysti</a>
+            </p>
+        </div>
+    </div>
+    <script src="js/popup.js"></script>
+</footer>
