414293df8a72440b053df34f0bb158c02262625d
Pixwok <40206880+Pixwok@users.noreply.github.com>
2019-04-24 11:47:19 +0200

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,14 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/nav.css">
+<nav id="nav">
+    <div class="bouton-menu">
+        <span></span>
+    </div>
+    <ul class="nav-content">
+        <li id="nav-link-img"><a href="https://wailcast.fr/"><img id="nav-logo" src="img/wlct_logo.png"></a></li>
+        <li class="nav-link"><a href="/index.php">ACCUEIL</a></li>
+        <li class="nav-link"><a href="/candidature.php">NOUS REJOINDRE</a></li>
+    </ul>
+</nav>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="js/nav.js"></script>
