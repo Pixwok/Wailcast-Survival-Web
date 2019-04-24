a5a3d745dc88bab35971980e5853810fb1afe8d9
Pixwok <40206880+Pixwok@users.noreply.github.com>
2019-04-24 11:45:56 +0200

    Create candidature.php

---
+++ candidature.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Wailcast-Survival</title>
+        <link rel="icon" type="image/png" href="img/wlct_logo.png">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138887128-3"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-138887128-3');
+        </script>
+    </head>
+
+    <body>
+        <?php include("nav.php"); ?>
+        <h1 id="title-candidature">Candidature</h1>
+        <p class="candidature-description">
+        Les candidatures pour le serveur Wailcast-Survival sont ouvertes à tous, peu importe vos compétences. 
+        <br>
+        Nous recherchons des joueurs sympathiques, actifs et motivés, qui comme nous, aiment  la survie sur Minecraft.
+        <br> 
+        Les compétences recherchées sont: redstone, build, programmation,... Cependant vous pouvez être accepté si vous n'avez pas ces compétences. 
+        </p>
+        <p class="candidature-description">
+        Votre candidature devra contenir:
+        <br>
+        - Une brève présentation de vous. <br>
+        - Vos compétences (screens, explication de vos projets). <br>
+        - Une présentation de vos expériences sur Minecraft.  <br>
+        - Soyez imaginatif pour votre candidature, surprenez-nous.
+        </p>
+        <p class="candidature-description">Envoyer votre candidature à <a id="mail" href="mailto:wailcast.py@gmail.com">wailcast.py@gmail.com</a></p>
+        <p class="candidature-description">Sur ce, nous espérons pouvoir jouer avec vous sur le serveur.</p>
+
+        <?php include("footer.php"); ?>
+    </body>
+</html>
