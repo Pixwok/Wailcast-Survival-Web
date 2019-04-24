615349934f23c1c1c77110a7bb3832b857d4dfd4
Pixwok <40206880+Pixwok@users.noreply.github.com>
2019-04-24 11:47:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Wailcast-Survival</title>
+        <link rel="icon" type="image/png" href="img/wlct_logo.png">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138887128-3"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-138887128-3');
+        </script>
+    </head>
+    
+    <body>
+            <?php include("nav.php"); ?>
+            <div class="slider">
+                <div id="slide">
+                    <img src="img/Slider-img-1.png">
+                    <img src="img/Slider-img-2.png">
+                    <img src="img/Slider-img-3.png">
+                </div>
+            </div>
+            <p id="description">
+            Wailcast-Survival est un serveur survie où règne avant tout la bonne entente entre les membres.<br> Ce serveur survie 100% Legit, a pour but de s’amuser en réalisant 
+            des projets tous ensemble. <br>
+            Le serveur est accessible sur candidature <a id="candidature" href="/candidature.php">ici</a>.
+            </p>
+            <h2 id="membre-title">Les Membres</h2>
+
+            <div id="membre-list">
+                <div class="membre">
+                    <img src="img/tete/Gaby_chr.png">
+                    Gaby_chr
+                </div>
+                <div class="membre">
+                    <img src="img/tete/Ziodyx.png">
+                    Ziodyx
+                </div>
+                <div class="membre">
+                    <img src="img/tete/Friegan.png">
+                    Friegan
+                </div>
+                <div class="membre">
+                    <img src="img/tete/Pixwok.png">
+                    Pixwok
+                </div>
+                <div class="membre">
+                    <img src="img/tete/Ysti.png">
+                    Ysti
+                </div>
+                <div class="membre">
+                    <img src="img/tete/Tekxinder.png">
+                    Tekxinder
+                </div>
+                <div class="membre">
+                    <img src="img/tete/kakkashii.png">
+                    Kakkashii
+                </div>
+                <div class="membre">
+                    <img src="img/tete/NL_Erwan.png">
+                    NL_Erwan
+                </div>
+                <div class="membre">
+                    <img src="img/tete/ZNeister.png">
+                    ZNeister
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+    </body>
+</html>
